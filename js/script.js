0f0231b67a54b2dce7446e933b21548521f08efb
Esteban Rios <166051721+EstebanRioss@users.noreply.github.com>
2025-04-11 01:10:41 -0300

    actualizacion del index

--- js/script.js
+++ js/script.js
@@ -98,3 +98,11 @@ document.addEventListener('DOMContentLoaded', function () {
     navMenu.classList.toggle('active');
   });
 });
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
